3414f0895717c23e3811479bbdd31c4eef4e16f8
Andrey Sitnik <andrey@sitnik.ru>
2015-08-09 02:42:44 +0300

    Remove unnecessary from cleaning from jsonify

--- jsonify.js
+++ jsonify.js
@@ -3,7 +3,6 @@ var path = require('path');
 function clean(node) {
     delete node.source.input.css;
     node.source.input.file = path.basename(node.source.input.file);
-    node.source.input.from = path.basename(node.source.input.from);
     if ( node.nodes ) node.nodes = node.nodes.map(clean);
     return node;
 }
