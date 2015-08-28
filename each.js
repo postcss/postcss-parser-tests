c165aece12c95123d5a8af20c3f53bc6040bf08b
Andrey Sitnik <andrey@sitnik.ru>
2015-08-29 00:12:51 +0300

    Trim JSON in cases

--- each.js
+++ each.js
@@ -10,7 +10,7 @@ var extra = require('./extra-cases');
 module.exports = function (callback) {
     fs.readdirSync(path.join(__dirname, 'cases')).filter(function (i) {
         if ( path.extname(i) !== '.json' ) return;
-        var json = read(i).toString();
+        var json = read(i).toString().trim();
         var name = path.basename(i, '.json');
         var css  = extra[name];
         if ( !css ) css = read(name + '.css').toString().trim();
