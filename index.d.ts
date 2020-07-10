5a74c0a05e581c0ca7855287b86aebf4be4faa9a
Andrey Sitnik <andrey@sitnik.ru>
2020-07-09 20:43:26 -0700

    Add types

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export function jsonify (node: Node): string
+
+export function path (name: string): string
+
+export function each (
+  callback: (name: string, css: string, json: string) => void
+): void
+
+export function real (
+  callback: (css: string) => { css: string }, extra: string[]
+): void
