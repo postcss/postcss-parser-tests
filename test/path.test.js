9469ac65ee1e212bcee04f90ab92842c60a64e04
Andrey Sitnik <andrey@sitnik.ru>
2020-07-09 21:09:11 -0700

    Fix tests

--- test/path.test.js
+++ test/path.test.js
@@ -1,7 +1,7 @@
 let { join } = require('path')
 
-let cases = require('../')
+let { testPath } = require('../')
 
 it('returns path', () => {
-  expect(cases.path('tab.css')).toEqual(join(__dirname, '../cases/tab.css'))
+  expect(testPath('tab.css')).toEqual(join(__dirname, '../cases/tab.css'))
 })
